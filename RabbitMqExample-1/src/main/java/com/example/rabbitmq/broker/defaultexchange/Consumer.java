b6c8761d0269baaac84e887aa0f787f4e5fd5341
apple <apple@192.168.0.104>
2019-09-12 21:32:59 +0530

    Direct exchange or also known as Default exchange works fine

--- RabbitMqExample-1/src/main/java/com/example/rabbitmq/broker/defaultexchange/Consumer.java
+++ RabbitMqExample-1/src/main/java/com/example/rabbitmq/broker/defaultexchange/Consumer.java
@@ -1,17 +1,13 @@
 package com.example.rabbitmq.broker.defaultexchange;
 
-import java.io.IOException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import com.example.rabbitmq.config.RabbitMqChannel;
-import com.example.rabbitmq.config.RabbitMqConnection;
 import com.example.rabbitmq.config.RabbitMqConnectionFactory;
-import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.DefaultConsumer;
-import com.rabbitmq.client.Envelope;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.DeliverCallback;
 
 @Component
 public class Consumer {
@@ -25,26 +21,29 @@ public class Consumer {
 	}
 
 	public void consumeMessage() throws Exception {
-		/* Note: connection and channel classes are auto closable */
-		try (RabbitMqConnection connection = new RabbitMqConnection(rabbitMqConnectionFactory.getConnectionFactory())) {
-			try (RabbitMqChannel channel = new RabbitMqChannel(connection.getConnection())) {
 
-				String queueName = "TestQueue";
-				channel.getChannel().queueDeclare(queueName, true, false, false, null);
+		Connection connection = rabbitMqConnectionFactory.getConnectionFactory().newConnection();
+		Channel channel = connection.createChannel();
 
-				com.rabbitmq.client.Consumer consumer = new DefaultConsumer(channel.getChannel()) {
-					@Override
-					public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
-							byte[] body) throws IOException {
-						String message = new String(body, "UTF-8");
-						System.out.println("Message " + message + " recieved from queue " + queueName);
-					}
-				};
+		String TASK_QUEUE_NAME = "TestQueue";
 
-				channel.getChannel().basicConsume(queueName, false, consumer);
+		channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+		channel.basicQos(1);
 
+		DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+			String message = new String(delivery.getBody(), "UTF-8");
+			System.out.println("Message " + message + " is consumed");
+			try {
+				// do any work with the message
+			} finally {
+				channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
 			}
-		}
+		};
+
+		boolean autoAck = false;
+		channel.basicConsume(TASK_QUEUE_NAME, autoAck, deliverCallback, consumerTag -> {
+		});
+
 	}
 
 }
--- RabbitMqExample-1/src/main/java/com/example/rabbitmq/broker/defaultexchange/Publisher.java
+++ RabbitMqExample-1/src/main/java/com/example/rabbitmq/broker/defaultexchange/Publisher.java
@@ -4,9 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import com.example.rabbitmq.config.RabbitMqChannel;
-import com.example.rabbitmq.config.RabbitMqConnection;
 import com.example.rabbitmq.config.RabbitMqConnectionFactory;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
 
 /*- Default exchange is the Direct exchange
  *  Queue name becomes the binding key
@@ -27,16 +27,14 @@ public class Publisher {
 
 	public void publishMessage() throws Exception {
 		/* Note: connection and channel classes are auto closable */
-		try (RabbitMqConnection connection = new RabbitMqConnection(rabbitMqConnectionFactory.getConnectionFactory())) {
-			try (RabbitMqChannel channel = new RabbitMqChannel(connection.getConnection())) {
-				String queueName = "TestQueue";
+		try (Connection connection = rabbitMqConnectionFactory.getConnectionFactory().newConnection()) {
+			try (Channel channel = connection.createChannel()) {
+				String TASK_QUEUE_NAME = "TestQueue";
 				for (int i = 0; i < 10; i++) {
 					String message = "This is message: #" + (i + 1);
 					/* Publish all the messages into the Queue */
-					channel//
-							.getChannel()//
-							.basicPublish("", queueName, null, message.getBytes());
-					LOGGER.info("Message " + message + " sent to queue " + queueName);
+					channel.basicPublish("", TASK_QUEUE_NAME, null, message.getBytes());
+					LOGGER.info("Message " + message + " sent to queue " + TASK_QUEUE_NAME);
 				}
 			}
 		}
